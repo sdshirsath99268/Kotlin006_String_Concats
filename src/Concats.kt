9a48294bd95bfe0d8dd9474225c7a0726b6726db
Sanjay Shirsath <sdshirsath@Sanjays-MacBook-Air.local>
2022-06-25 21:06:16 +0530

    Initial commit

---
+++ src/Concats.kt
@@ -0,0 +1,10 @@
+fun main(args:Array<String>) {
+    val first = "Sanjay "
+    val second = "Devidas "
+    val third = "Shirsath"
+
+    val result = first + second + third
+    println(result)
+
+    println("Sixth Kotlin Prog , By Sanjay D Shirsath")
+}
